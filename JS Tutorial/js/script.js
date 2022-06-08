6125fee38ec536aee3951f25ba99a90189fe1440
vsaxena12 <vsaxena1@binghamton.edu>
2022-06-07 20:18:07 -0500

    pushing the latest code

---
+++ JS Tutorial/js/script.js
@@ -0,0 +1,18 @@
+alert('Hello World');
+
+let storeFromPrompt = prompt('Hello World');
+
+if (storeFromPrompt > 10) {
+    document.writeln("What's Up\n");
+} else {
+    document.writeln("Fuck Off\n");
+}
+
+
+let compare = confirm(45);
+
+if (compare) {
+    document.writeln("Compared");
+} else {
+    document.writeln("Not Compared");
+}
